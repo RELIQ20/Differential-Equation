070b1ddfdb6284106a7ababb040d956d0d1cd84e
Nath <nathaniellawas05@gmail.com>
2024-12-19 16:53:07 +0800

    header-icon change

--- Differential_Equation/src/Header/Header.jsx
+++ Differential_Equation/src/Header/Header.jsx
@@ -1,21 +1,24 @@
 import './Header.css';
-function Header(){
-    return(
+import letterN from '../../assets/letter-n.png';
+import infoButton from '../../assets/information-button.png';
+
+function Header() {
+    return (
         <>
-        <header>
-            <div className="header-left">
-                <img src="./src/assets/letter-n.png" alt="Nathaniel" />
-                <h1 className="headtitle">DIFFERENTIAL EQUATION</h1>
-            </div>
-            <nav className="navmain">
-                <ul className="navUL">
-                    <li><a href="" target="_blank">About</a></li>
-                    <a href="" target="_blank"><img src="./src/assets/information-button.png" id="info"></img></a>
-                </ul>
-            </nav>
-        </header>
-      </>
-    ); 
+            <header>
+                <div className="header-left">
+                    <img src={letterN} alt="Nathaniel" />
+                    <h1 className="headtitle">DIFFERENTIAL EQUATION</h1>
+                </div>
+                <nav className="navmain">
+                    <ul className="navUL">
+                        <li><a href="" target="_blank">About</a></li>
+                        <a href="" target="_blank"><img src={infoButton} id="info" alt="Info" /></a>
+                    </ul>
+                </nav>
+            </header>
+        </>
+    );
 }
 
-export default Header
+export default Header;
